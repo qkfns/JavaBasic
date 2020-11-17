aae60c9c19230159e1289421e3cf98137adc5229
qkfns <qkfns@naver.com>
2020-11-17 11:47:13 +0900

    선생님이 작성한 코드

--- src/qkfns/SungJukV1.java
+++ src/qkfns/SungJukV1.java
@@ -11,40 +11,40 @@ package qkfns;
  * 만점은 100점으로, 10점단위로 학점
  * 100~90, 89~80, 79~70, 69~60, 59~0
  * name, kor, eng , mat, sum, mean, grd
+ *
+ * 삼항연산자 : (조건식) ?  참일때결과값 : 거짓일때결과값
  */
 public class SungJukV1 {
     public static void main(String[] args) {
-        String name;
-        int kor=99,eng=98,mat=99;
-        double sum,mean;
-        name="홍길동";
-        sum = kor+eng+mat;
-        mean= (double)sum/3;
+        //변수선언
+        String name = "지현";
+        int kor = 99;
+        int eng = 98;
+        int mat = 99;
+        int sum = 0;
+        double mean = 0.0;
+        char grd = '가';
 
+        //처리
+        sum= kor + eng + mat;
+        mean = (double)sum / 3;
+        grd =(mean >= 90) ? '수':
+                (mean >= 80) ? '우': (mean >= 80) ? '우':
+
+
+
+
+
+
+        //결과출력
         System.out.println("이름: "+name);
-        System.out.println("국어점수: "+kor);
-        System.out.println("영어점수: "+eng);
-        System.out.println("수학점수: "+mat);
-        System.out.println("평균점수: "+mean);
-
-        if (mean>=90){
-            System.out.println("학점: 수");
-        }
-        if (mean<90&&mean>=80){
-            System.out.println("학점: 우");
-        }
-        if (mean<80&&mean>=70){
-            System.out.println("학점: 미");
-        }
-        if (mean<70&&mean>=60){
-            System.out.println("학점: 양");
-        }
-        if (mean<60&&mean>=0){
-            System.out.println("학점: 가");
-        }
-        if (mean<0){
-            System.out.println("오류입니다. 점수를 다시 확인해주세요");
-        }
+        System.out.println("국어: "+kor);
+        System.out.println("영어: "+eng);
+        System.out.println("수학: "+mat);
+        System.out.println("----------------");
+        System.out.println("총점: "+sum);
+        System.out.println("평균: "+mean);
     }
 }
+
 
