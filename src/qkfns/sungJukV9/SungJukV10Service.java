606084b8d52c42f1edef115a1fc084109090edb6
qkfns <qkfns@naver.com>
2020-11-27 15:20:44 +0900

    자바 입출력 스트림 학습

--- src/qkfns/sungJukV9/SungJukV10Service.java
+++ src/qkfns/sungJukV9/SungJukV10Service.java
@@ -21,12 +21,21 @@ public class SungJukV10Service extends SungJukV9Service{
     private BufferedReader br = null;
     private BufferedWriter bw = null;
 
+    public SungJukV10Service() {
+        // 프로그램 최초시작시
+        // 파일에 저장된 모든 성적테이터를
+        // List 타입의 sjdata변수에 저장
+        try {
+            loadSungJuk();
+        } catch (IOException e) {
+            System.out.println("성적 데이터 초기화중 오류발생");
+            e.printStackTrace();
+        }
+    }
+
     //멤버변수 선언
     //입력받은 모든 성적데이터를 저장하는 동적배열 변수 선언
     List<SungJukVO> sjdata = new ArrayList<>();
-
-
-
 
     /**
      *  성적 처리 프로그램의 메뉴 출력 기능
@@ -74,68 +83,34 @@ public class SungJukV10Service extends SungJukV9Service{
         fw = new FileWriter(fpath, true);
         bw = new BufferedWriter(fw);
         String data = String.format(fmt,sj.getName(),sj.getKor(),sj.getEng(),sj.getMat(),sj.getSum(),sj.getMean(),sj.getGrd());
-        bw.newLine();
         bw.write(data);
-
+        bw.newLine();   //줄바꿈 메서드
+        // FileWriter 클래스를 이용해서
+        // 파일에 데이터를 기록할때 다소 불편함
+        // =>PrintWriter 추천!
         bw.close();
         fw.close();
     }
 
-    public void computeSungJuk(SungJukVO sj){
-        sj.setSum ( sj.getKor() + sj.getEng() + sj.getMat());
-        sj.setMean( (double)sj.getSum() / 3)  ;
-        switch((int)(sj.getMean() / 10)){
-            case 10:
-            case 9: sj.setGrd('수'); break;
-            case 8: sj.setGrd('우'); break;
-            case 7: sj.setGrd('미'); break;
-            case 6: sj.setGrd('양'); break;
-            default: sj.setGrd('가');
-        } //switch
-    } //computeSungJuk
 
-    @Override
     /**
-     * 저장된 성적 데이터들 중에서
-     * 번호, 이름, 국어, 영어, 수학만 뽑아서 리스트형태로 출력
+     *  loadSungJuk
+     *  파일에 저장된 성적데이터를 List타입의 sjdata에 저장
      */
-    public void readSungJuk() {
-        String fmt = "이름: %s, 국어: %d, 영어: %d, 수학: %d\n";
-        // 동적 배열에 저장된 데이터들을 출력하기 위해
-        // 각 요소를 순회할 수 있도록 Iterator 객체 선언
-        Iterator<SungJukVO> iter = sjdata.iterator();
-        while(iter.hasNext()){
-            SungJukVO sj = iter.next();
 
-            System.out.printf(fmt,sj.getName(),sj.getKor(),sj.getEng(),sj.getMat());
+    protected void loadSungJuk() throws IOException {
+        fr = new FileReader(fpath);
+        br = new BufferedReader(fr);
+
+        while(br.ready()){
+            String s[] = br.readLine().split(",");
+
+            sj = new SungJukVO(s[0], Integer.parseInt(s[1]), Integer.parseInt(s[2]), Integer.parseInt(s[3]), Integer.parseInt(s[4]),
+                    Double.parseDouble(s[5]), s[6].charAt(0));
+            sjdata.add(sj);
+
         }
+        br.close();
+        fr.close();
     }
-
-    @Override
-    /**
-     *  상세조회할 학생이름을 입력받아
-     *  이름, 국어, 영어, 수학, 총점, 학점을 출력한다
-     */
-    public void readOneSungJuk() {
-        String fmt = "이름: %s, 국어: %d, 영어: %d, 수학: %d, 총점: %d, 평균: %.1f,학점: %s\n";
-        //상세조회할 학생이름 입력 받음
-        Scanner sc = new Scanner(System.in);
-        System.out.println("조회할 학생이름은? ");
-        String name = sc.nextLine();
-
-        // 입력받은 이름으로 데이터 검색후 결과 출력
-        for(SungJukVO sj : sjdata){
-            if(sj.getName().equals(name)){
-                System.out.printf(fmt,sj.getName(),sj.getKor(),sj.getMat(),sj.getEng(),sj.getSum(),sj.getMean(),sj.getGrd());
-                break;
-            }
-        }
-
-    }
-
-
-
-
-
-
 }
